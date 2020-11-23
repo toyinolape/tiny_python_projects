88f8c56b9223302bd59ce596512a61a582115b4f
Toyin O <toyinolape@yahoo.com>
2020-11-23 22:11:04 +0100

    Code on Working with files and STDOUT

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -10,9 +10,10 @@ import os
 import sys
 import io
 
+
 # --------------------------------------------------
 def get_args():
-    """Get command-line arguments"""
+    """get command-line arguments"""
 
     parser = argparse.ArgumentParser(
         description='Howler Exercise',
@@ -20,6 +21,7 @@ def get_args():
 
     parser.add_argument('text',
                         metavar='text',
+                        type=str,
                         help='Input string or file')
 
     parser.add_argument('-o',
@@ -32,20 +34,24 @@ def get_args():
     args = parser.parse_args()
 
     if os.path.isfile(args.text):
-        args.text = open(args.text).read().rstrip()
-
+        args.text = open(args.text)
+    else:
+        args.text = io.StringIO(args.text + '\n')
 
     return args
+
+
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Convert to uppercase"""
 
     args = get_args()
-    howler = args.text
-    out_fh = open(args.outflie, "wt") if args.outfile else sys.stdout
-    out_fh.write(howler.upper())
+    out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
+    for line in args.text:
+        out_fh.write(line.upper())
     out_fh.close()
-     
+
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
